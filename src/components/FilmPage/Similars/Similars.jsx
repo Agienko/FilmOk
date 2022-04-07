87049f1ac6e7c7430393925068d476398c71e1a2
Yuriy <Agienkoyura@gmail.com>
2022-04-07 17:07:25 +0300

    slider

--- src/components/FilmPage/Similars/Similars.jsx
+++ src/components/FilmPage/Similars/Similars.jsx
@@ -1,17 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import CardLittle from "../../CardLittle/CardLittle";
-
+import arrow from "../../../img/arrow.png"
 import s from "./Similars.module.css"
 
 const Similars = props => {
-   
+    const STEP = 185 //card width
+    const [overY, setOverY] = useState(0)
+
+const leftClick = () => (overY < 0) ? setOverY(overY + STEP*2) : setOverY(0)
+
+const rightClick = () =>{
+    (overY > -185*(props.similars.length-1)) 
+    ? setOverY(overY - STEP*2)
+    : setOverY(-STEP*(props.similars.length-1))
+}
+
+const animation = {
+    transition: 'transform .18s ease-out',
+    transform: `translateX(${overY}px)`
+}
+
     return (
             <div className={s.similarsWrapp}>
                 <h2> Похожие фильмы:</h2>
                     <div className={s.similars}>
-                        {props.similars.map(i =>
-                        <CardLittle image={i.posterUrlPreview} name={i.nameRu} id={i.filmId} key={i.filmId}/>
-                        )}
+                        <div className={s.arrowWrapLeft} onClick={leftClick}> 
+                            <img src={arrow} alt="left" />
+                        </div>
+                       <div className={s.content} style={animation}>
+                            {props.similars.map(i =>
+                                <CardLittle image={i.posterUrlPreview} name={i.nameRu} 
+                                id={i.filmId} key={i.filmId}/>
+                                )}
+                       </div>
+                        
+
+                        <div className={s.arrowWrapRight} onClick={rightClick}> 
+                            <img src={arrow} alt="right" />
+                        </div>
                     </div>
             </div>
                        
